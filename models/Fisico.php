cf25bb29aa68a48af74d175c65f6349835d917fb
Marcos <mariro@alumnatflorida.es>
2026-04-23 09:27:56 +0200

    CLase principal

---
+++ models/Fisico.php
@@ -0,0 +1,5 @@
+<?php
+
+class Fisico extends Juego {
+    
+}
---
+++ models/Juego.php
@@ -0,0 +1,30 @@
+<?php
+
+abstract class Juego {
+    protected $nombre;
+    protected $tipo;
+    protected $plataforma;
+    protected $genero;
+    protected $estado;
+    protected $puntuacion;
+    protected $reseña;
+
+
+    public function __construct($nombre, $tipo, $plataforma, $genero, $puntuacion, $reseña) {
+        $this->nombre = $nombre;
+        $this->tipo = $tipo;
+        $this->plataforma = $plataforma;
+        $this->genero = $genero;
+        $this->puntuacion = $reseña;
+        $this->reseña = $reseña;
+    }
+
+    public function getNombre(){ return $this->nombre; }
+    public function getTipo(){ return $this->tipo; }
+    public function getPlataforma(){ return $this->plataforma; }
+    public function getGenero(){ return $this->genero; }
+    public function getEstado(){ return $this->estado; }
+    public function getPuntuacion(){ return $this->puntuacion; }
+    public function getReseña(){ return $this->reseña; }
+
+}
